7ba814b9fd70fa9265612ae56d1fc0e46b609db2
davydovsky <davydovs59@gmail.com>
2015-12-02 17:12:16 +0300

    Добавлен основной файл js

---
+++ js/App.js
@@ -0,0 +1,12 @@
+/**
+ * @author      Nikita Davydovsky   
+ * @since       2015-12-01          (the version of the package this class was first added to)
+ */
+
+var app = app || {};
+
+$(function () {
+    'use strict';
+    
+    new app.MainView();
+});
